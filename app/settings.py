2e1d75aa82c7e8bf88ea9932a0fbe06885d142b2
Andres Vargas <andrevargas@coca-cola.com>
2020-03-24 00:21:49 -0600

    update

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i^^fqx+hcei4m+nn@8@&&%-u_5sg9%+aucwk_$9l-47oi+)+g5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','192.168.99.101']
 
 
 # Application definition
