4fbad91d6923c97b6c729d8ce5d0db36dcd9ec4a
Andres Vargas <andrevargas@coca-cola.com>
2020-06-14 18:40:51 -0500

    adding styles

--- livewire/templatetags/livewire_tags.py
+++ livewire/templatetags/livewire_tags.py
@@ -4,6 +4,10 @@ from livewire.utils import instance_class
 
 register = template.Library()
 
+@register.inclusion_tag("livewire_styles.html", takes_context=True)
+def livewire_styles(context):
+    return context
+
 
 @register.inclusion_tag("livewire_scripts.html", takes_context=True)
 def livewire_scripts(context):
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="livewire",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
 )
